76af745c11d577efb4c939c2832efaabba08f067
FX <marian.focsa@outlook.com>
2016-04-26 01:06:06 +0300

    Added project schema
    Generated all the classes needed from UML Diagrams (including a 'dummy' Representation class).
    Created the Java Project containing the classes (and packages).
    Removed any aventual errors.

---
+++ Image_Processor/src/com/imgprocessor/processor/ExtendedImage.java
@@ -0,0 +1,58 @@
+package com.imgprocessor.processor;
+
+import javax.swing.ImageIcon;
+
+/**
+ * 
+ */
+public class ExtendedImage extends ImageIcon {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * 
+     */
+    public ImageState state;
+
+    /**
+     * 
+     */
+    public int totalWidth;
+
+    /**
+     * 
+     */
+    public int totalHeight;
+
+    /**
+     * 
+     */
+    public int truncWidth;
+
+    /**
+     * 
+     */
+    public int truncHeight;
+
+    /**
+     * 
+     */
+    public int truncX;
+
+    /**
+     * 
+     */
+    public int truncY;
+    
+    /**
+     * Default constructor
+     */
+    public ExtendedImage() {
+    	
+    }
+
+
+}
